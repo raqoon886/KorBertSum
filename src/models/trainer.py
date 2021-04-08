4e594d359d0ba505f1a39f95680be90672e6ac6b
raqoon886 <wieder886@gmail.com>
2021-04-08 23:47:28 +0900

    update summary function

--- src/models/trainer.py
+++ src/models/trainer.py
@@ -300,6 +300,91 @@ class Trainer(object):
 
         return stats
 
+    def summary(self, test_iter, step, cal_lead=False, cal_oracle=False):
+        """ Validate model.
+            valid_iter: validate data iterator
+        Returns:
+            :obj:`nmt.Statistics`: validation loss statistics
+        """
+        # Set model in validating mode.
+        def _get_ngrams(n, text):
+            ngram_set = set()
+            text_length = len(text)
+            max_index_ngram_start = text_length - n
+            for i in range(max_index_ngram_start + 1):
+                ngram_set.add(tuple(text[i:i + n]))
+            return ngram_set
+
+        def _block_tri(c, p):
+            tri_c = _get_ngrams(3, c.split())
+            for s in p:
+                tri_s = _get_ngrams(3, s.split())
+                if len(tri_c.intersection(tri_s))>0:
+                    return True
+            return False
+
+        if (not cal_lead and not cal_oracle):
+            self.model.eval()
+        stats = Statistics()
+
+        
+        with torch.no_grad():
+            for batch in test_iter:
+                src = batch.src
+                labels = batch.labels
+                segs = batch.segs
+                clss = batch.clss
+                mask = batch.mask
+                mask_cls = batch.mask_cls
+
+
+                gold = []
+                pred = []
+
+                if (cal_lead):
+                    selected_ids = [list(range(batch.clss.size(1)))] * batch.batch_size
+                elif (cal_oracle):
+                    selected_ids = [[j for j in range(batch.clss.size(1)) if labels[i][j] == 1] for i in
+                                    range(batch.batch_size)]
+                else:
+                    sent_scores, mask = self.model(src, segs, clss, mask, mask_cls)
+
+                    loss = self.loss(sent_scores, labels.float())
+                    loss = (loss * mask.float()).sum()
+                    batch_stats = Statistics(float(loss.cpu().data.numpy()), len(labels))
+                    stats.update(batch_stats)
+
+                    sent_scores = sent_scores + mask.float()
+                    sent_scores = sent_scores.cpu().data.numpy()
+                    selected_ids = np.argsort(-sent_scores, 1)
+                # selected_ids = np.sort(selected_ids,1)
+                for i, idx in enumerate(selected_ids):
+                    _pred = []
+                    if(len(batch.src_str[i])==0):
+                        continue
+                    for j in selected_ids[i][:len(batch.src_str[i])]:
+                        if(j>=len(batch.src_str[i])):
+                            continue
+                        candidate = batch.src_str[i][j].strip()
+                        if(self.args.block_trigram):
+                            if(not _block_tri(candidate,_pred)):
+                                _pred.append(candidate)
+                        else:
+                            _pred.append(candidate)
+
+                        if ((not cal_oracle) and (not self.args.recall_eval) and len(_pred) == 3):
+                            break
+
+                    _pred = '<q>'.join(_pred)
+                    if(self.args.recall_eval):
+                        _pred = ' '.join(_pred.split()[:len(batch.tgt_str[i].split())])
+
+                    pred.append(_pred)
+                    gold.append(batch.tgt_str[i])
+
+                print(' '.join(pred), ' '.join(gold))
+
+        return pred
 
 
     def _gradient_accumulation(self, true_batchs, normalization, total_stats,
